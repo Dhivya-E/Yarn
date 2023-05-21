205cdbf791e9fbe109d6bd57e20b3d8e6c4bf08c
Dhivya-E <dhivyait25@gmail.com>
2023-05-21 22:55:26 +0530

    Updates

---
+++ precache-manifest.e18d5c08e62129c9dd910198ebf52c7a.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f4736fe13dbd5d94819d4888f7fe18",
+    "url": "/Yarn/index.html"
+  },
+  {
+    "revision": "0fefc460130aef358a36",
+    "url": "/Yarn/static/css/2.f53c7e07.chunk.css"
+  },
+  {
+    "revision": "f1f5183ccdfc40032bbf",
+    "url": "/Yarn/static/css/main.fc7426f5.chunk.css"
+  },
+  {
+    "revision": "0fefc460130aef358a36",
+    "url": "/Yarn/static/js/2.b3568001.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Yarn/static/js/2.b3568001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f5183ccdfc40032bbf",
+    "url": "/Yarn/static/js/main.38f7b14f.chunk.js"
+  },
+  {
+    "revision": "cc1ba837f2673f820c2d",
+    "url": "/Yarn/static/js/runtime-main.38ff7557.js"
+  },
+  {
+    "revision": "52bd6fc7a91099030950c40ce5e96ee5",
+    "url": "/Yarn/static/media/homeimg.52bd6fc7.jpg"
+  },
+  {
+    "revision": "640593c55239d38d1da77d5f72aff2b1",
+    "url": "/Yarn/static/media/img-6.640593c5.jpg"
+  },
+  {
+    "revision": "b639816ab1f56e91ac37f34dd8dba510",
+    "url": "/Yarn/static/media/img2.b639816a.jpg"
+  },
+  {
+    "revision": "8958f855a5983b346fc6d8113f8f7b81",
+    "url": "/Yarn/static/media/img3.8958f855.jpg"
+  },
+  {
+    "revision": "ce801ec6cede6eebbd69dbbadf65787c",
+    "url": "/Yarn/static/media/img4.ce801ec6.jpg"
+  },
+  {
+    "revision": "e2afa3a0d7c904b9a69407fb1accc95c",
+    "url": "/Yarn/static/media/img5.e2afa3a0.jpg"
+  },
+  {
+    "revision": "6c51d684ce34c2db54a61be28ae4553c",
+    "url": "/Yarn/static/media/img6.6c51d684.jpg"
+  },
+  {
+    "revision": "1ca7ae7463cd1b00b25cf448250cd994",
+    "url": "/Yarn/static/media/img_1.1ca7ae74.jpg"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/Yarn/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/Yarn/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/Yarn/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/Yarn/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/Yarn/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/Yarn/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/Yarn/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/Yarn/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/Yarn/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/Yarn/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/Yarn/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/Yarn/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/Yarn/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/Yarn/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/Yarn/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/Yarn/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
